c25a29b33693655be6fde367274ae183567ec450
Quertte <a.injen90@gmail.com>
2020-03-07 21:10:17 +0300

    Двухмерный массив. Вывод на консоль.[#239026](с 1 до 9 - поправил)

--- chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
+++ chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
@@ -4,14 +4,14 @@ public class BarleyBreak {
 
     public static void main(String[] args) {
         int[][] num = new int[3][3];
-        num[0][0] = 0;
-        num[0][1] = 1;
-        num[0][2] = 2;
-        num[1][0] = 3;
-        num[1][1] = 4;
-        num[1][2] = 5;
-        num[2][0] = 6;
-        num[2][1] = 7;
-        num[2][2] = 8;
+        num[0][0] = 1;
+        num[0][1] = 2;
+        num[0][2] = 3;
+        num[1][0] = 4;
+        num[1][1] = 5;
+        num[1][2] = 6;
+        num[2][0] = 7;
+        num[2][1] = 8;
+        num[2][2] = 9;
     }
 }
