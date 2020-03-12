0bfd0decd18654944f6b69826c30e153558c53e0
Quertte <a.injen90@gmail.com>
2020-03-12 13:15:44 +0300

    Объединить два массива [#124482]

---
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -0,0 +1,39 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Merge {
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rs1 = process.merge(
+                new int[]{1, 3, 5},
+                new int[]{2, 4}
+        );
+        System.out.println(Arrays.toString(rs1));
+    }
+
+    public int[] merge(int[] left, int[] right) {
+        int[] rs1 = new int[left.length + right.length];
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < rs1.length; i++) {
+            if (l < left.length && r < right.length) {
+                if (left[l] < right[r]) {
+                    rs1[i] = left[l];
+                    l++;
+                } else {
+                    rs1[i] = right[r];
+                    r++;
+                }
+            } else if (l < left.length) {
+                rs1[i] = left[l];
+                l++;
+            } else if (r < right.length) {
+                rs1[i] = right[r];
+                r++;
+            }
+        }
+        return rs1;
+    }
+}
