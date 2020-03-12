e8c7fa259e559294e7f774df7896c6baaf89a15c
Quertte <a.injen90@gmail.com>
2020-03-12 22:54:57 +0300

    Объединить два массива [#124482] (Строки упростил)

--- chapter_001/src/main/java/ru/job4j/sort/Merge.java
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -20,18 +20,14 @@ public class Merge {
         for (int i = 0; i < rs1.length; i++) {
             if (l < left.length && r < right.length) {
                 if (left[l] < right[r]) {
-                    rs1[i] = left[l];
-                    l++;
+                    rs1[i] = left[l++];
                 } else {
-                    rs1[i] = right[r];
-                    r++;
+                    rs1[i] = right[r++];
                 }
             } else if (l < left.length) {
-                rs1[i] = left[l];
-                l++;
+                rs1[i] = left[l++];
             } else if (r < right.length) {
-                rs1[i] = right[r];
-                r++;
+                rs1[i] = right[r++];
             }
         }
         return rs1;
