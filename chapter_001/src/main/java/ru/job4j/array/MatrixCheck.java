79bcc0c3fe81e0975c2918a138952a401607b565
Quertte <a.injen90@gmail.com>
2020-05-07 13:10:40 +0300

    Нарисовать ромб, прямоугольник,Прямоугольный треулголник, равносторонний треугольник

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -35,7 +35,7 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int j = 0; j < board.length; j++) {
-            if(board[j][j] == 'X'){
+            if (board[j][j] == 'X') {
                 if (monoHorizontal(board, j) || monoVertical(board, j)) {
                     result = true;
                     break;
